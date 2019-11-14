8aca53a6a9cf11f5d5f28476654b219b389fd6e9
marksdreamwall <andrey.dreamwall@gmail.com>
2019-11-14 14:58:29 +0300

    Создание файла .my.cnf

---
+++ create_my_cnf.sh
@@ -0,0 +1,10 @@
+andrey@andrey-VirtualBox:~$ vim .my.cnf
+andrey@andrey-VirtualBox:~$ cat .my.cnf
+[client]
+user=root
+password=*******
+
+
+andrey@andrey-VirtualBox:~$ chmod 0600 ~/.my.cnf
+andrey@andrey-VirtualBox:~$ mysql
+mysql> 
